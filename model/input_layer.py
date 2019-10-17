Correct backpropagation

model/input_layer.py
@@ -1,7 +1,11 @@
 import sys
+import logging
 import numpy as np
 from model.layer import Layer
 from config import IMAGE_SIZE
+from utils import normalize_pixel_value
+
+logging.basicConfig(level=logging.INFO)
 
 
 class InputLayer(Layer):
@@ -16,14 +20,16 @@ class InputLayer(Layer):
         '''
         input_array = []
         for row in matrix:
-            input_array.extend(row)
-        return input_array
+            input_array.extend(
+                [normalize_pixel_value(val) for val in row]
+            )
+        return np.array(input_array, ndmin=2)
 
     def reset_image(self, new_image):
-        self.nodes.append(self._distribute_image(new_image))
+        self.nodes = self._distribute_image(new_image)
 
     def set_error(self, matrix):
-        print('Back Propagation Complete')
+        pass
 
     def __len__(self):
         return IMAGE_SIZE

model/layer.py
@@ -8,8 +8,8 @@ class Layer(object, metaclass=ABCMeta):
         self.nodes = []
         self.error_matrix = []
 
-    def set_node_values(self, vector):
-        self.nodes.append(vector)
+    def set_node_values(self, matrix):
+        self.nodes = matrix
 
     def reset_node_values(self):
         self.nodes = []
@@ -18,7 +18,7 @@ class Layer(object, metaclass=ABCMeta):
         self.error_matrix = []
             
     def get_node_array(self):
-        return np.array(self.nodes[-1])
+        return np.array(self.nodes)
 
     def __str__(self):
         return ', '.join([str(node) for node in self.nodes])

model/output_layer.py
@@ -3,7 +3,7 @@ import random
 import operator
 import logging
 import numpy as np
-from utils import squared_error
+from utils import error
 from model.layer import Layer
 from config import NUM_OUTPUT_NODES
 
@@ -17,30 +17,23 @@ class OutputLayer(Layer):
         self.expected_output = None
         self.error_matrix = []
 
-    # def _build_output_nodes(self):
-    #     return [0] * NUM_OUTPUT_NODES
-
     def set_expected_output(self, number):
         self.expected_output = int(number)
 
     def set_error_matrix(self):
-        output_values = self.get_node_array()
+        output_values = self.get_node_array()[0]
         error_matrix = []
         for i in range(len(output_values)):
             if i == self.expected_output:
-                error_matrix.append(squared_error(1, output_values[i]))
+                error_matrix.append(error(1, output_values[i]))
             else:
-                error_matrix.append(squared_error(0, output_values[i]))
-        # logging.info(f'Appending Error Matrix: {[(error/ sum(error_matrix)) for error in error_matrix]}')
-        self.error_matrix.append(
-            np.array([(error/ sum(error_matrix)) for error in error_matrix])
-        )
-                
+                error_matrix.append(error(0, output_values[i]))
+        self.error_matrix = np.array(error_matrix)
+
     def get_classification(self):
-        node_array = self.get_node_array()
+        node_array = list(self.get_node_array()[0])
         return node_array.index(max(node_array))
 
-    # Kind of Hacky but it solves the issue of length within the WeightMatrix w/o Abstract Classes
     def __len__(self):
         return NUM_OUTPUT_NODES
 

model/weight_matrix.py
@@ -13,19 +13,22 @@ class WeightMatrix:
     def __init__(self, inlayer=None, outlayer=None):
         self.inlayer = inlayer
         self.outlayer = outlayer
-        self.weights = np.random.random((len(inlayer), len(outlayer)))
+        self.weights = np.random.normal(
+            loc=0, scale=((len(inlayer)+len(outlayer))**(-0.5)), size=(len(inlayer), len(outlayer))
+        )
 
     def propagate_forward(self):
         ''' Matrix Multiplication to forward propagate '''
         if not self.inlayer or not self.outlayer:
             sys.exit('NoneType layer detected')
-
         node_values = self.inlayer.get_node_array()
         output_values = node_values.dot(self.weights)
-        self.outlayer.set_node_values(output_values)
+        self.outlayer.set_node_values(
+            sigmoid(output_values)
+        )
 
     def propagate_backward(self):
-        ''' Use Sigmoid and MSE to back propagate '''
+        ''' Use stochastic gradient descent to backpropagate '''
         if not self.inlayer or not self.outlayer:
             sys.exit('NoneType layer detected')
 
@@ -37,6 +40,9 @@ class WeightMatrix:
         )
 
         # compute new weight matrix
-        self.weights = stochastic_gradient_descent(
-            error_matrix, np.array(self.inlayer.nodes), self.weights
+        w_prime = stochastic_gradient_descent(
+            error_matrix, self.inlayer.get_node_array(), 
+            self.outlayer.get_node_array(), self.weights
         )
+
+        self.weights = w_prime

utils.py
@@ -6,38 +6,33 @@ from config import LEARNING_RATE
 logging.basicConfig(level=logging.INFO)
 
 
+def normalize_pixel_value(pixel):
+    val = pixel/255
+    if val == 1:
+        return 0.99
+    elif val == 0:
+        return 0.01
+    else:
+        return val
+
+
 def sigmoid(x):
-    return 1 / (1 + math.exp(-x))
+    return 1 / (1 + np.exp(-x))
 
 
-# def sigmoid_for_array(arr):
-#     return 1/(1 * np.exp(arr))
+def stochastic_gradient_descent(error, prev_layer, layer, weights):
+
+    # compute gradient
+    gradient = prev_layer.T.dot(
+        error * (layer * (1 - layer))
+    )
+    
+    # scale by learning rate
+    scaled_gradient = LEARNING_RATE * gradient
+
+    # return new weights
+    return weights - scaled_gradient
 
 
-# def derivative_sigmoid(x):
-#     return sigmoid_for_array(x) * (1 - sigmoid_for_array(x))
-
-
-# def stochastic_gradient_descent(error, prev_layer, weights):
-#     gradient = prev_layer.dot(
-#         error * derivative_sigmoid(prev_layer.dot(weights))
-#     )
-#     scaled_gradient = np.multiply(LEARNING_RATE, gradient)
-#     return np.add(weights, scaled_gradient)
-
-def sigmoid_for_array(arr):
-    return 1/(1 * np.exp(arr))
-
-
-def derivative_sigmoid(x):
-    return sigmoid_for_array(x) * (1 - sigmoid_for_array(x))
-
-
-def stochastic_gradient_descent(error, prev_layer, weights):
-    gradient = np.dot(np.array(prev_layer).T, error * np.array(derivative_sigmoid(prev_layer.dot(weights))))
-    scaled_gradient = np.multiply(LEARNING_RATE, gradient)
-    return np.add(weights, scaled_gradient)
-
-
-def squared_error(target, output):
-    return (target - output)**2
+def error(target, output):
+    return output - target